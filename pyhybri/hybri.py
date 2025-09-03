fix float32 overflow

pyhybri/hybri.py
@@ -1,7 +1,21 @@
 import scipy.signal
 import hrir_builder as hrb
-from hybri_tools import CoordMode, PolarPoint, BuildMode, InterpolationDomain, AirData, CurveMode, HBuilded, RBuilded, intermediate_segment, CartesianPoint, LinearTrajectory, ParametricTrajectory
-from hybri_tools import AngleMode # noqa
+from hybri_tools import (
+    CoordMode,
+    PolarPoint,
+    BuildMode,
+    InterpolationDomain,
+    AirData,
+    CurveMode,
+    HBuilded,
+    RBuilded,
+    intermediate_segment,
+    AngleMode, # noqa: F401
+    CartesianPoint, # noqa: F401
+    LinearTrajectory, # noqa: F401
+    ParametricTrajectory, # noqa: F401
+    linear_direction_info # noqa: F401
+)
 from hrir_builder import HInfo
 import rir_builder as rib
 import numpy as np
@@ -16,17 +30,17 @@ SOFT_CLIP_SCALE = 1.0 / 0.707
 BUFFER_MAX_SIZE = 88200
 
 class SmoothedConvolution():
-    
+
     @staticmethod
     def apply_intermediate(x: NDArray[np.float64], prev_kernel: NDArray[np.float64]|None, curr_kernel: NDArray[np.float64], transition_length: int) -> NDArray[np.float64]:
         if prev_kernel is None:
             return scipy.signal.fftconvolve(x, curr_kernel, mode="full")
-        
+
         ksize = max(prev_kernel.size, curr_kernel.size)
         total_size = x.size + ksize - 1
         prev_kernel = np.pad(prev_kernel, (0, ksize - prev_kernel.size), mode="constant")
         kernel_padded = np.pad(curr_kernel, (0, ksize - curr_kernel.size), mode="constant")
-        
+
         transition_length = min(transition_length, total_size)
         smoothed = intermediate_segment(x=x, k1=prev_kernel, k2=kernel_padded, ksize=ksize, tlength=transition_length)
         if transition_length < x.size:
@@ -36,18 +50,18 @@ class SmoothedConvolution():
 
 class HybriParams():
     def __init__(
-        self, 
-        hrir_database_path: str, 
-        rir_database_path: str | None, 
-        coord_mode: CoordMode, 
-        interp_domain: InterpolationDomain, 
-        build_mode: BuildMode, 
-        chunk_size: int = 1024, 
-        interpolation_neighs: int = 3, 
+        self,
+        hrir_database_path: str,
+        rir_database_path: str | None,
+        coord_mode: CoordMode,
+        interp_domain: InterpolationDomain,
+        build_mode: BuildMode,
+        chunk_size: int = 1024,
+        interpolation_neighs: int = 3,
         sample_rate: float = 44100,
         gamma: float = 1.0
     ) -> None:
-        
+
         """
         HYBRIZONE INIT PARAMETERS
 
@@ -95,12 +109,12 @@ class RTOverlapSaveBufferConvolution():
         self.pkernel = kernel.copy()
 
         convolution[:self.chunk] += self.buffer[:self.chunk]
-        
+
         self.buffer = np.roll(self.buffer, -self.chunk)
         self.buffer[-self.chunk:] = 0.0
         convolved = convolution[:self.chunk]
         tail_size = convolution.size - self.chunk
-        
+
         if tail_size > 0:
             if tail_size > self.init_buffer_size:
                 temp_buffer = np.zeros(tail_size, np.float64)
@@ -121,11 +135,11 @@ class Hybrizone():
 
     def __init__(self, params: HybriParams) -> None:
         self.__params = params
-        
+
         self.hrir_builder = hrb.HRIRBuilder(
-            hrir_database=params.hrir_database_path, 
-            mode=params.coord_mode, 
-            interp_domain=params.interpolation_domain, 
+            hrir_database=params.hrir_database_path,
+            mode=params.coord_mode,
+            interp_domain=params.interpolation_domain,
             gamma=params.gamma
         )
 
@@ -133,7 +147,7 @@ class Hybrizone():
         self.__rir_buffer = None
         if self.__params.rir_database_path is not None:
             self.rir_builder = rib.RIRMorpha(
-                rir_database_path=str(params.rir_database_path), 
+                rir_database_path=str(params.rir_database_path),
                 source_distance=self.hrir_builder.dataset.get_source_distance(),
                 gamma=params.gamma
             )
@@ -141,11 +155,11 @@ class Hybrizone():
 
         self.__hrir_left_buffer = RTOverlapSaveBufferConvolution(chunk=self.__params.chunk_size)
         self.__hrir_right_buffer = RTOverlapSaveBufferConvolution(chunk=self.__params.chunk_size)
-        
+
         self.__temp_hrir = None
         self.__temp_rho = None
         self.__temp_rir = None
-        
+
         self.__htime = 0.0
         self.__rtime = 0.0
         self.__ptime = 0.0
@@ -163,12 +177,12 @@ class Hybrizone():
         self.__temp_hrir = None
         self.__temp_rho = None
         self.__temp_rir = None
-        
+
         self.__htime = 0.0
         self.__rtime = 0.0
         self.__ptime = 0.0
         self.__counter = 1
-        
+
         print("[INFO] Hybrizone closed!")
 
     def process_frame(self, frame: NDArray[np.float64], kernels: HybriKernels) -> NDArray[np.float32]:
@@ -199,7 +213,9 @@ class Hybrizone():
             left_hrir = lc.result()
             right_hrir = lr.result()
 
-        convolved = np.column_stack((left_hrir, right_hrir)).astype(np.float32)
+        convolved = np.column_stack((left_hrir, right_hrir))
+        convolved = np.clip(convolved, np.finfo(np.float32).min, np.finfo(np.float32).max)
+        convolved = convolved.astype(np.float32)
         tend = time.perf_counter()
         self.__ptime += tend - tstart
         self.__counter += 1
@@ -211,26 +227,26 @@ class Hybrizone():
         temp_hrir = self.build_distance_based_hrir(hrirs=hrirs)
         tend = time.perf_counter()
         self.__htime += tend - tstart
-        
+
         self.__temp_hrir = temp_hrir
         self.__temp_rho = position.rho
-    
+
     def set_morph_data(self, direction: float, morph_curve: CurveMode) -> None:
         tstart = time.perf_counter()
         self.__temp_rir = self.build_hybrid_space(direction=direction, morph_curve=morph_curve, rho=self.__temp_rho)
         tend = time.perf_counter()
         self.__rtime += tend - tstart
-        
+
     def get_kernels(self) -> HybriKernels:
         return HybriKernels(rir=self.__temp_rir, hrir=self.__temp_hrir.hrir, itd=self.__temp_hrir.itd, gain=self.__temp_hrir.gain)
-    
+
     def get_proc_times(self) -> dict:
         return {
             "htime": self.__htime / self.__counter,
             "rtime": self.__rtime / self.__counter,
             "ptime": self.__ptime / self.__counter
         }
-    
+
     # --- START HRIR SECTION ---
 
     def imposed_air_conditions(self, air_data: AirData) -> None:
@@ -278,9 +294,9 @@ class Hybrizone():
 
         Returns
         -------
-        HBuilded : 
+        HBuilded :
             hrir, itd
-        
+
         """
 
         return self.hrir_builder.build_hrir(hrirs_info=hrirs, method=self.__params.build_mode)
@@ -386,5 +402,5 @@ class Hybrizone():
     def get_rir_data(self, rir: NDArray[np.float64]) -> RBuilded:
         data = self.rir_builder._get_data_plot(rir=rir)
         return RBuilded(rir=rir, power_spectrum=data.mag, freqs=data.f, integr=data.integr)
-    
+
     # --- END RIR SECTION ---

pyhybri/hybri_tools.py
@@ -42,13 +42,13 @@ class BuildMode(Enum):
 class InterpolationDomain(Enum):
     TIME = 0
     FREQUENCY = 1
-        
+
 class CartesianPoint():
     def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.z = z
         self.y = y
-        
+
     def to_polar(self):
         rho = np.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
         rho = max(rho, ETA)
@@ -361,18 +361,18 @@ class LinearTrajectory():
             at index [0] x = front-back meters
             at index [1] y = left-right meters
             at index [2] z = up-down meters
-        
-        direction : NDArray[float] 
+
+        direction : NDArray[float]
         """
-        
+
         self.cpa = cpa
         norm = np.linalg.norm(direction)
         if norm == 0:
             print("[ERROR] Direction vector must be not zero")
             exit(1)
-            
+
         self.direction = direction / norm
-    
+
     def get_points(self, distances: NDArray[np.float32]) -> list[PolarPoint]:
         point = self.cpa + self.direction * distances[:, None] # Formula: P0 + V * steps
         points = []
@@ -391,7 +391,7 @@ class ParametricTrajectory():
         for p in stack:
             points.append(CartesianPoint(p[0], p[1], p[2]).to_polar())
         return points
-    
+
     @staticmethod
     def get_parabolic_points(total_distance: float, total_duration: float, max_elevation: float, t: NDArray[np.float32]) -> list[PolarPoint]:
         t_norm = t - (total_duration / 2)
@@ -403,3 +403,14 @@ class ParametricTrajectory():
         for p in stack:
             points.append(CartesianPoint(p[0], p[1], p[2]).to_polar())
         return points
+
+def linear_direction_info(direction: NDArray[np.float32]) -> None:
+    rho = np.linalg.norm(direction)
+    if rho == 0:
+        print("[ERROR] Zero norm!")
+        exit(1)
+    azi = np.rad2deg(np.arctan2(direction[1], direction[0]))
+    ele = np.rad2deg(np.arcsin(direction[2] / rho))
+    print("[DIRECTION INFO]")
+    print(f"[AZIMUTH VARIATION] = {azi:.3f}\u00B0")
+    print(f"[ELEVATION VARIATION] = {ele:.3f}\u00B0")

pyhybri/rt_test.py
@@ -1,5 +1,19 @@
 # import section
-from hybri import Hybrizone, HybriParams, PolarPoint, AirData, AngleMode, CoordMode, BuildMode, CurveMode, InterpolationDomain, CartesianPoint, LinearTrajectory, ParametricTrajectory
+from hybri import (
+    Hybrizone,
+    HybriParams,
+    PolarPoint, # noqa F401
+    AirData,
+    AngleMode, # noqa F401
+    CoordMode,
+    BuildMode,
+    CurveMode,
+    InterpolationDomain,
+    CartesianPoint, # noqa F401
+    LinearTrajectory,
+    ParametricTrajectory,
+    linear_direction_info
+)
 import numpy as np
 import pyaudio as pa
 from enum import Enum
@@ -13,27 +27,49 @@ RIR_PATH = "/Users/pm/AcaHub/Coding/BinauralSpatial/data/RIR-MIT_SURVEY.h5"
 AUDIO_PATH = "/Users/pm/AcaHub/Coding/BinauralSpatial/audio_examples"
 
 class AudioExample(Enum):
-    HELICOPTER = 1,
+    HELICOPTER1 = 1,
     GUN_FIRE = 2,
     RUNNING = 3
-    
+    HELICOPTER2 = 4,
+    FOOTSTEP = 5,
+    JET = 6,
+    WALKING_GRASS = 7,
+    SOVIET_GUNFIRE = 8,
+    GUNSHOT = 9,
+    STAIRS = 10
+
+
 def load_audio_example(audio: AudioExample):
     ex = ""
     match audio:
-        case AudioExample.HELICOPTER:
-           ex = "helicopter.wav"
+        case AudioExample.HELICOPTER1:
+           ex = "helicopter1.wav"
+        case AudioExample.HELICOPTER2:
+           ex = "helicopter2.wav"
+        case AudioExample.FOOTSTEP:
+           ex = "footstep.wav"
+        case AudioExample.JET:
+           ex = "jet.wav"
         case AudioExample.GUN_FIRE:
            ex = "gun_fire.wav"
         case AudioExample.RUNNING:
            ex = "running.wav"
+        case AudioExample.WALKING_GRASS:
+           ex = "walking-grass.wav"
+        case AudioExample.SOVIET_GUNFIRE:
+           ex = "soviet-gunfire.wav"
+        case AudioExample.GUNSHOT:
+           ex = "gunshot.wav"
+        case AudioExample.STAIRS:
+           ex = "stairs.wav"
         case _:
             print("[ERROR] Wrong example")
             exit(1)
-        
+
     path_to_example = os.path.join(AUDIO_PATH, ex)
     s = wave.open(path_to_example, "r")
     return s
-    
+
 CHUNK = 1024
 SR = 44100
 CHANNELS = 2
@@ -52,8 +88,8 @@ PARAMS = HybriParams(
 
 # main function
 def main() -> None:
-    AUDIO_SIGNAL = load_audio_example(audio=AudioExample.RUNNING)
-    frame_block = AUDIO_SIGNAL.readframes(CHUNK)
+    # AUDIO_SIGNAL = load_audio_example(audio=AudioExample.FOOTSTEP)
+    # frame_block = AUDIO_SIGNAL.readframes(CHUNK)
 
     # start hybri
     AURALIZER = Hybrizone(params=PARAMS)
@@ -63,51 +99,62 @@ def main() -> None:
     AURALIZER.imposed_air_conditions(air_data=air_conditions)
 
     # pass the rirs
-    AURALIZER.set_rirs(rir1=10, rir2=70, smooth_factor=0.1)
+    AURALIZER.set_rirs(rir1=36, rir2=39, smooth_factor=0.1)
 
     # audio engine
     PORTAUDIO = pa.PyAudio()
     stream = PORTAUDIO.open(format=pa.paFloat32, channels=CHANNELS, rate=SR, output=True, frames_per_buffer=CHUNK)
     stream.start_stream()
 
-    nblocks = AUDIO_SIGNAL.getnframes() // CHUNK
-    max_distance = 100
-    
-        
     # IN-HEAD
     # distances = np.abs(np.linspace(1, -1, nblocks, endpoint=True)) * max_distance
     # azimuths = np.zeros(nblocks)
     # elevations = np.zeros(nblocks)
-    
+
     # FIXED_AZI = 0.0 # IN-FRONT
     # FIXED_PHI = 40 # IN-FRONT
-    
-    # azimuths[:nblocks // 2 + 1] = FIXED_AZI 
+
+    # azimuths[:nblocks // 2 + 1] = FIXED_AZI
     # azimuths[nblocks // 2 + 1:] = FIXED_AZI + 180 # invert azi to back FLY-BY
     # elevations[:nblocks // 2 + 1] = FIXED_PHI
     # elevations[nblocks // 2 + 1:] = -FIXED_PHI # invert ele to back
-    
-    
-    hel_cpa = np.array([0.0, -3.0, 0.0]) # closest point to me 
-    # hel_cpa = np.array([0.0, 0.0, 5.0]) # closest point to me fli-by
-    hel_direction = np.array([-1.0, 0.0, 0.0])
-    hel_distances = np.linspace(-max_distance, max_distance, nblocks, endpoint=True)
-    hel_linear_traj = LinearTrajectory(cpa=hel_cpa, direction=hel_direction)
-    hel_points = hel_linear_traj.get_points(distances=hel_distances)
-    
-    omega = np.linspace(0.1, 1.5, nblocks, endpoint=True)
-    times = np.linspace(0, nblocks * CHUNK / SR, nblocks, endpoint=True)
-    distances_circular = np.linspace(10, 1, nblocks, endpoint=True)
-    vertical_vel = np.zeros(nblocks)
-    # vertical_vel[:nblocks // 2 + 1] = 0.5
-    # vertical_vel[nblocks // 2 + 1:] = -0.5
-    hel_circular_path = ParametricTrajectory.get_circular_points(omega=omega, t=times, radius=distances_circular, start_elevation=0.0, vertical_vel=vertical_vel)
-    parabolic = ParametricTrajectory.get_parabolic_points(total_distance=max_distance, total_duration=nblocks * CHUNK / SR, max_elevation=30, t=times)
 
-    fixed_point = PolarPoint(rho=10, phi=25, theta=270, opt=AngleMode.DEGREE)
-    
-    
-    mode = "fixed"
+    AUDIO_SIGNAL = load_audio_example(audio=AudioExample.WALKING_GRASS)
+    frame_block = AUDIO_SIGNAL.readframes(CHUNK)
+    nblocks = AUDIO_SIGNAL.getnframes() // CHUNK
+
+    MAX_DISTANCE = 30
+    example_mode = "linear_traj"
+
+    points = None
+    match example_mode:
+        case "linear_traj":
+            cpa = np.array([1.0, 0.0, 0.0]) # closest point (left Y pos, in front X pos, up Z pos)
+            direction = np.array([0.0, -1.0, 0.0])
+
+            linear_direction_info(direction=direction)
+
+            distances = np.linspace(-MAX_DISTANCE / 2, MAX_DISTANCE / 2, nblocks, endpoint=True)
+            
+            space_step = MAX_DISTANCE / (nblocks - 1)
+            sec = space_step / (CHUNK / SR) 
+            print(f"[VELOCITY] = {sec:.3f} m/s")
+            
+            linear_traj = LinearTrajectory(cpa=cpa, direction=direction)
+            points = linear_traj.get_points(distances=distances)
+        case "circular_traj" | "parabolic_traj":
+            omega = np.linspace(0.1, 1.5, nblocks, endpoint=True)
+            times = np.linspace(0, nblocks * CHUNK / SR, nblocks, endpoint=True)
+            distances_circular = np.linspace(10, 1, nblocks, endpoint=True)
+            vertical_vel = np.zeros(nblocks)
+            # vertical_vel[:nblocks // 2 + 1] = 0.5
+            vertical_vel[nblocks // 2 + 1:] = -0.5
+
+            if example_mode == "circular_traj":
+                points = ParametricTrajectory.get_circular_points(omega=omega, t=times, radius=distances_circular, start_elevation=0.0, vertical_vel=vertical_vel)
+            else:
+                points = ParametricTrajectory.get_parabolic_points(total_distance=MAX_DISTANCE, total_duration=nblocks * CHUNK / SR, max_elevation=30, t=times)
+
     
     RUN = True
     index = 0
@@ -118,32 +165,18 @@ def main() -> None:
             #     current_rho = distances[index]
             #     current_phi = elevations[index]
             #     current_theta = azimuths[index]
-                if mode == "linear":
-                    pos = hel_points[index]
-                elif mode == "circular":
-                    pos = hel_circular_path[index]
-                elif mode == "parabolic":
-                    pos = parabolic[index]
-                elif mode == "fixed":
-                    pos = fixed_point
+                pos = points[index]
             else:
             #     current_rho = distances[-1]
             #     current_phi = elevations[-1]
             #     current_theta = azimuths[-1]
-                if mode == "linear":
-                    pos = hel_points[-1]
-                elif mode == "circular":
-                    pos = hel_circular_path[-1]
-                elif mode == "parabolic":
-                    pos = parabolic[-1]
-                elif mode == "fixed":
-                    pos = fixed_point
-            
+                pos = points[-1]
+
             frame = np.frombuffer(frame_block, dtype=np.int16) # mono
             frame = frame / 32768.0
-            
+
             print(f"rho: [{pos.rho}], phi: [{pos.phi}], theta: [{pos.theta}]")
-            
+
             # pos = PolarPoint(rho=current_rho, phi=current_phi, theta=current_theta, opt=AngleMode.DEGREE)
             AURALIZER.set_position(position=pos)
 
@@ -167,7 +200,6 @@ def main() -> None:
 
     while RUN and frame_block != b'':
         pass
-    
 
     AURALIZER.close()
     stream.stop_stream()